3e29532c08e658e1a8262c763901faa509e1604d
MChavez2901 <chavezm17060@17060m.cghs.crusaders>
2015-09-10 18:59:36 -0400

    9/10 push bot tele op update
    added "easy mode, in addition to turn speed control to increase
    driveability

--- PushBotTeleOp.java
+++ PushBotTeleOp.java
@@ -27,47 +27,87 @@ public class PushBotTeleOp extends OpMode{
     float leftPower, rightPower;
     DcMotor rightMotor, leftMotor, armMotor;
     Servo lServo, rServo;
-    double minL, maxL, minR, maxR;
+    double minL = 0;
+    double maxL = 1;
+    double minR = 0;
+    double maxR = 1;
     double targetValue;
     double lPosition, rPosition;
     double lOffset, rOffset;
+    double armSpeed = 1;
+    double moveSpeed = 1;
+    int loopnumber;
+    int marker;
+
     public void loop()
     {
         // collects joystick values from the left and right joystick to set motor values
         float leftThrottle = gamepad1.left_stick_y;
         float rightThrottle = gamepad1.right_stick_y;
-        leftPower = leftThrottle;
-        rightPower = rightThrottle;
-        rightMotor.setPower(rightPower);
-        leftMotor.setPower(leftPower);
+        leftPower = leftThrottle ;
+        rightPower = rightThrottle ;
+        if (Math.abs(rightPower) > .05 || Math.abs(leftPower) > .05)
+        {
+            if ( (Math.abs(rightPower) > .5 ) && rightPower + leftPower < .2) //"if the motors are moving at a substantial speed in opposite direction"
+            {
+                // slows down the motors while turning
+                rightMotor.setPower(rightPower* moveSpeed*.5);
+                leftMotor.setPower(leftPower* moveSpeed*.5);
+                telemetry.addData("3", "Turn Speed Control activated");
+            }else {
+                // if the motors are not turning, motors will turn at their intended speed
+                rightMotor.setPower(rightPower * moveSpeed);
+                leftMotor.setPower(leftPower * moveSpeed);
+                telemetry.addData("3", "Turn Speed Control NOT activated");
+
+            }
+        }
+
         telemetry.addData("1", leftThrottle);
         telemetry.addData("2", rightThrottle);
+        if (gamepad1.right_bumper && (loopnumber > 15 ) )
+        { // marker tells the program what mode the robot is currently in, easy or fast
+            if (marker == 0) {
+                armSpeed = .25;
+                moveSpeed = .5;
+                marker = 1;
+            }
+            if (marker == 1)
+            {
+                armSpeed = 1;
+                moveSpeed = 1;
+                marker = 0;
+            }
+            loopnumber = 0;
+        }
         if (gamepad1.a)
         {
-            armMotor.setPower(-.5);
+            armMotor.setPower(-.5 * armSpeed);
         }
         if (gamepad1.b)
         {
-            armMotor.setPower(.5);
+            armMotor.setPower(.5 * armSpeed);
         }
         if (!gamepad1.a && !gamepad1.b)
         {
             armMotor.setPower(0);
         }
 
-        if (gamepad1.left_trigger > 0 && targetValue <= 1)
+        if (gamepad1.left_trigger > 0 && targetValue <= .9)
         {
             targetValue += .005 * (gamepad1.left_trigger + .1);
         }
-        if (gamepad1.right_trigger > 0 && targetValue >= 0)
+        if (gamepad1.right_trigger > 0 && targetValue >= .1)
         {
             targetValue += -.005 * (gamepad1.right_trigger + .1);
         }
-        telemetry.addData("1", targetValue);
-        lPosition = targetValue + lOffset;
+        telemetry.addData("1", moveSpeed);
+        telemetry.addData("2", armSpeed);
+        lPosition = -targetValue + 1;
         rPosition = targetValue  + rOffset;
         lServo.setPosition(lPosition);
         rServo.setPosition(rPosition);
+        loopnumber++;
 
     }
 }
